967afe94ffe04804fc0ba49a4fa53b5a9e6af9c0
prunik-96 <168493482+prunik-96@users.noreply.github.com>
2025-05-23 11:53:45 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,4 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-const socket = io();
-
-let players = {};
-let me = null;
+let bullets = [];
 
 document.addEventListener('keydown', e => {
   if (!me) return;
@@ -13,6 +8,7 @@ document.addEventListener('keydown', e => {
   if (e.key === 'ArrowRight') me.angle += 0.1;
   if (e.key === 'a') me.turretAngle -= 0.1;
   if (e.key === 'd') me.turretAngle += 0.1;
+  if (e.code === 'Space') socket.emit('shoot');
 
   socket.emit('move', me);
 });
@@ -21,34 +17,37 @@ function drawTank(player) {
   ctx.save();
   ctx.translate(player.x, player.y);
   ctx.rotate(player.angle);
-  ctx.fillStyle = 'green';
+  ctx.fillStyle = player.id === socket.id ? 'green' : 'blue';
   ctx.fillRect(-15, -10, 30, 20);
   ctx.restore();
 
-  // башня
   ctx.save();
   ctx.translate(player.x, player.y);
   ctx.rotate(player.turretAngle);
   ctx.fillStyle = 'gray';
   ctx.fillRect(0, -5, 30, 10);
   ctx.restore();
+
+  // HP bar
+  ctx.fillStyle = 'red';
+  ctx.fillRect(player.x - 20, player.y - 30, 40, 5);
+  ctx.fillStyle = 'lime';
+  ctx.fillRect(player.x - 20, player.y - 30, 40 * (player.hp / 100), 5);
 }
 
-socket.on('currentPlayers', data => {
-  players = data;
-  me = players[socket.id];
-});
+function drawBullets() {
+  for (let b of bullets) {
+    ctx.save();
+    ctx.translate(b.x, b.y);
+    ctx.rotate(b.angle);
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(0, -2, 10, 4);
+    ctx.restore();
+  }
+}
 
-socket.on('newPlayer', player => {
-  players[player.id] = player;
-});
-
-socket.on('updatePlayer', player => {
-  players[player.id] = player;
-});
-
-socket.on('removePlayer', id => {
-  delete players[id];
+socket.on('updateBullets', data => {
+  bullets = data;
 });
 
 function gameLoop() {
@@ -56,7 +55,6 @@ function gameLoop() {
   for (let id in players) {
     drawTank(players[id]);
   }
+  drawBullets();
   requestAnimationFrame(gameLoop);
 }
-
-gameLoop();
